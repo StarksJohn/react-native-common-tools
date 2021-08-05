aaa7591696b426aca1754404d0e6ebb160bdfe1a
stark <397866153@qq.com>
2021-08-05 15:46:00 +0800

    add eslint

--- tools/asyncStorage.js
+++ tools/asyncStorage.js
@@ -1,6 +1,6 @@
-import AsyncStorage from '@react-native-community/async-storage';
-import * as stringTools from './stringTools';
-import tool from './tool';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import * as stringTools from './stringTools'
+import tool from './tool'
 /**
  *
  * @param key
@@ -9,16 +9,16 @@ import tool from './tool';
  */
 const setItem = async (key, value) => {
   try {
-    console.log('asyncStorage.js setItem key=', key, ' value=', value);
-    console.log('asyncStorage.js setItem key= typeof value=', typeof value);
+    console.log('asyncStorage.js setItem key=', key, ' value=', value)
+    console.log('asyncStorage.js setItem key= typeof value=', typeof value)
     if (typeof value === 'string' && stringTools.isNull(value)) {
       // alert('setItem 参数不是 str 类型,value=', value)
       // console.log('setItem 参数不是 str 类型,value=', value, ' key=', key)
-      console.log('asyncStorage.js setItem key=', key, ' value不存在');
-      return Promise.reject('setItem value不存在');
+      console.log('asyncStorage.js setItem key=', key, ' value不存在')
+      return Promise.reject('setItem value不存在')
     } else if (typeof value !== 'string') {
-      console.log('asyncStorage.js setItem value !== string');
-      let v = JSON.stringify(value);
+      console.log('asyncStorage.js setItem value !== string')
+      const v = JSON.stringify(value)
 
       // if (value instanceof Array) {
       //   //数组类型
@@ -29,7 +29,7 @@ const setItem = async (key, value) => {
       //   v = JSON.stringify(value);
       // }
       if (!stringTools.isNull(v)) {
-        const [err, res] = await tool.to(AsyncStorage.setItem(key, v));
+        const [err, res] = await tool.to(AsyncStorage.setItem(key, v))
         console.log(
           'asyncStorage.js setItem key=',
           key,
@@ -38,38 +38,38 @@ const setItem = async (key, value) => {
           ' res=',
           res,
           ' err=',
-          err,
-        );
+          err
+        )
 
         if (err) {
-          return Promise.reject(err);
+          return Promise.reject(err)
         } else {
-          return Promise.resolve(true);
+          return Promise.resolve(true)
         }
       }
     } else {
-      console.log('asyncStorage.js setItem key=', key, ' value=', value);
-      const [err, res] = await tool.to(AsyncStorage.setItem(key, value));
+      console.log('asyncStorage.js setItem key=', key, ' value=', value)
+      const [err, res] = await tool.to(AsyncStorage.setItem(key, value))
       if (err) {
-        return Promise.reject(err);
+        return Promise.reject(err)
       } else {
-        return Promise.resolve(true);
+        return Promise.resolve(true)
       }
     }
   } catch (e) {
     // saving error
     // throw new Error(`缓存 key=${key}的 失败`);
-    return Promise.reject(`缓存 key=${key}的 失败`);
+    return Promise.reject(`缓存 key=${key}的 失败`)
   }
-};
+}
 
 const removeItem = async (key) => {
-  await AsyncStorage.removeItem(key);
-};
+  await AsyncStorage.removeItem(key)
+}
 
 const getItem = async (key) => {
   try {
-    const value = await AsyncStorage.getItem(key);
+    const value = await AsyncStorage.getItem(key)
     if (value !== null) {
       // value previously stored
       if (stringTools.isJsonStr(value)) {
@@ -77,32 +77,32 @@ const getItem = async (key) => {
           'asyncStorage.js getItem成功 && 是json字符串, key=',
           key,
           '  value=',
-          JSON.parse(value),
-        );
-        return Promise.resolve(JSON.parse(value));
+          JSON.parse(value)
+        )
+        return Promise.resolve(JSON.parse(value))
       } else {
         console.log(
           'asyncStorage.js getItem成功 && 不是 json 字符串, key=',
           key,
           '  value=',
-          value,
-        );
-        return Promise.resolve(value);
+          value
+        )
+        return Promise.resolve(value)
       }
     } else {
-      console.log(`未找到 key=${key}的 缓存`);
-      return Promise.reject(null);
+      console.log(`未找到 key=${key}的 缓存`)
+      return Promise.reject(null)
     }
   } catch (e) {
     // error reading value
     // throw new Error(`未找到 key=${key}的 缓存`)
-    console.log(`未找到 key=${key}的 缓存`);
-    return Promise.reject(null);
+    console.log(`未找到 key=${key}的 缓存`)
+    return Promise.reject(null)
   }
-};
+}
 
 export default {
   setItem,
   removeItem,
-  getItem,
-};
+  getItem
+}
