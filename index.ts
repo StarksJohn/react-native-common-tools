0243f6cf5bed6a0ccca6534813cf6ee8b122b50d
cham1985 <397866153@qq.com>
2021-11-25 16:29:04 +0800

    no message

--- index.ts
+++ index.ts
@@ -1,44 +1,9 @@
 import { ahooks, arrayTools, dateTools, Math, objTools, stringTools, tool } from 'starkfrontendtools'
-// import baseTimer from './tools/baseTimer'
 import EventListener, { sendEvent } from './tools/EventListener'
-// import * as screenTools from './tools/screenTools'
 import appStyle from './styles/appStyle'
-// import ViewPropTypes from './components/ViewPropTypes'
-// import Text from './components/Text/Text'
-// import Button from './components/Button'
-// import PureComponent from './components/PureComponent'
-// import HttpManager from './api/HttpManager'
-// import * as netwrokCode from './api/netwrokCode'
-// import FlatList from './components/FlatList'
 import constant from './constant/constant'
-// import SectionList from './components/SectionList'
-// import homeSpringBoxQueue from './tools/homeSpringBoxQueue'
-// import Toast from './components/Toast' //
-// import SearchInput from './components/SearchInput'
-// import VerificationCodeBt from './components/VerificationCodeBt'
-// import TextBt from './components/TextBt'
-// import ImgBt from './components/ImgBt'
-// import clipboardTools from './tools/clipboardTools'
-// import BasePureComponent from './components/BasePureComponent'
-// import Banner from './components/Banner'
-// import GridView from './components/GridView'
-// import FullScreenLoading from './components/FullScreenLoading'
-// // import md5 from './tools/md5'
-// import md5 from './tools/blueimp_md5'
-// import useAppStateListener from './components/CustomHooks/useAppStateListener'
 import useAndroidBackHandler from './components/CustomHooks/useAndroidBackHandler'
-// import useSubscribeKeyboard from './components/CustomHooks/useSubscribeKeyboard'
 import asyncStorage from './tools/asyncStorage'
-// import {
-//   XHttp,
-//   XImage,
-//   XText,
-//   XView,
-//   XWidget,
-//   XSize /* 取代dp */,
-//   XTSize /* 适配不同屏幕的字体,等比例缩放之 */,
-//   ResetStyle
-// } from 'react-native-easy-app'
 import routes, { _RouteProps } from './routes/index'
 import MyStyleSheet from './styles/MyStyleSheet'
 import { List, one_section_array } from './components/List/index'
